1fbd59c7bac08b7a142a9d26c6d44e09b8ce8ae4
Joe Racz <raczonline@gmail.com>
2021-12-21 15:35:13 -0500

    Initial input component

---
+++ src/components/form-elements/Input/Input.vue
@@ -0,0 +1,27 @@
+<template>
+  <input
+    class="w-full"
+    :type="type"
+    :value="modelValue"
+    @input="$emit('update:modelValue', $event)"
+  />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Input",
+  props: {
+    modelValue: {
+      type: [String, Number],
+      default: '',
+    },
+    type: {
+      type: String,
+      required: true
+    }
+  },
+  emits: ['update:modelValue']
+})
+</script>
